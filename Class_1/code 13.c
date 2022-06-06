cc2af2a1e2b98b552cda5f0830414c4b6a855baf
MD Nazmul Hossan <105975139+naz1084@users.noreply.github.com>
2022-06-06 15:56:56 +0600

    Add files via upload

---
+++ Class_1/code 13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int number,result;
+    printf("enter a five digit of number=");
+    scanf("%d",&number);
+
+    result=number+11111;
+
+    printf("result=%d\n",result);
+    return 0;
+}
