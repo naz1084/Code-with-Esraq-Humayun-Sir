a18ae5e93fa1f0ab5bc7fd12ca07dfc4068aa1d2
MD Nazmul Hossan <105975139+naz1084@users.noreply.github.com>
2022-06-06 16:35:07 +0600

    Add files via upload

---
+++ Lab Assignment/code 5.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(){
+    int i,j,k,row,n;
+    printf("Input number of row:");
+    scanf("%d",&row);
+    n=row-1;
+    for(i=1;i<=row;i++){
+        for(j=n;j>=1;j--){
+            printf(" ");
+        }
+        for(k=1;k<=i;k++){
+            printf("* ");
+        }
+            printf("\n");
+            n--;
+
+    }
+    return 0;
+}
