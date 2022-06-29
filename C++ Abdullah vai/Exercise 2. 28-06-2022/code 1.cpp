9b6ff334890bd791946fce87a788475c776b352d
MD Nazmul Hossan <105975139+naz1084@users.noreply.github.com>
2022-06-29 12:54:47 +0600

    Add files via upload

---
+++ C++ Abdullah vai/Exercise 2. 28-06-2022/code 1.cpp
@@ -0,0 +1,21 @@
+//this is sqrt code
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    //this is input variable
+    int number;
+    //this is output variable
+    double root;
+    //this is enter line
+    cout<<"enter any number=";
+    //this is user input line
+    cin>>number;
+    //this is math term
+    root=sqrt(number);
+    //this is result of root
+    cout<<"root="<<root<<endl;
+
+    return 0;
+}
